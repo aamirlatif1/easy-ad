e1caff021bc19f5e1aae979eb3f6f9cabb3e3243
Muhammad Mehran <mehran.shabir@hotmail.com>
2021-03-28 16:19:47 +0500

    added list api (#2)

--- src/main/java/com/ufo/easyad/web/AdController.java
+++ src/main/java/com/ufo/easyad/web/AdController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class AdController {
 
@@ -13,4 +15,8 @@ public class AdController {
         return new Ad(adId, "Test", "test desc");
     }
 
+    @GetMapping("/ads")
+    public List<Ad> getAds(){
+        return List.of(new Ad(1, "Test", "test desc"));
+    }
 }
