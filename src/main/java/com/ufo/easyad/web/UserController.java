37ca9580e3294cc753f3dcf42d6c1c938a4687fd
Aamir Latif <aamirlatif@gmail.com>
2021-03-28 12:50:55 +0200

    Added username and password as a params

--- src/main/java/com/ufo/easyad/web/UserController.java
+++ src/main/java/com/ufo/easyad/web/UserController.java
@@ -2,13 +2,14 @@ package com.ufo.easyad.web;
 
 import com.ufo.easyad.domain.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
     @GetMapping("/users/login")
-    public User login(){
-        return new User(1, "test", "test");
+    public User login(@RequestParam("username")String username, @RequestParam("password") String password) {
+        return new User(1, username, password);
     }
 }
